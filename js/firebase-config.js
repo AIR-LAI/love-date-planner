6c0494230f143448b350b1a6a764be18a3371bde
AIR-LAI <141419159+AIR-LAI@users.noreply.github.com>
2026-07-11 17:53:37 +0800

    Add js/firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+﻿// ============================================
+// Firebase 配置
+// ============================================
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCzVLw1F-Pl-LMKpQ97LfPmAFVnAFHBdTc",
+  authDomain: "loveu-90411.firebaseapp.com",
+  projectId: "loveu-90411",
+  storageBucket: "loveu-90411.firebasestorage.app",
+  messagingSenderId: "423410264755",
+  appId: "1:423410264755:web:aa132e41e6562631f7dec6",
+  measurementId: "G-P4CQL274T9"
+};
